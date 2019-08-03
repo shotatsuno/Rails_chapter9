270ec8704bca4c33f220706c25f2c48f4874de47
Shota Tsuno <shotatsuno@JcrawfordMacbookPro.local>
2019-08-03 19:51:07 +0900

    [finish]3-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'cfa6429e1a722c7c88f7e1b9e156d5aeae2dbc1f338c2f22169a4eb79c7ac3de6ad00640b9bbc36ac4c826b7fc1516b9306c3d765332bf95ce76459ffa149b0a'
+
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  get 'posts/new'
+  get 'top' => 'root#top'
+    
+  post 'posts' => 'posts#create'
+  
+  get 'posts' => 'posts#index'
+    
+  get 'posts/:id' => 'posts#show', as: 'post'
+    
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+    
+  patch 'posts/:id' => 'posts#ipdate', as: 'update_post'
+    
+  delete 'posts/:id' => 'posts#destroy' , as: 'destroy_post'
+    
+end
